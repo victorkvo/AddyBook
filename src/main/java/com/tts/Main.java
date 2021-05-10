709c5315cd62960c4218bf2ff4a5c1016860cf5d
Victor Vo <79146455+victorkvo@users.noreply.github.com>
2021-05-10 15:19:09 -0500

    couple updates

--- src/main/java/com/tts/Main.java
+++ src/main/java/com/tts/Main.java
@@ -83,8 +83,9 @@ public class Main extends Entry {
                     "Please choose what you'd like to do with the database:\n");
             entryInput = sc.nextInt();
             if (entryInput == 1) {
-                Entry newEntry = new Entry();
+                // Add an entry
 
+                Entry newEntry = new Entry();
                 System.out.println("Enter first name: ");
                 newEntry.setfName(sc.next());
                 System.out.println("Enter last name: ");
@@ -98,22 +99,30 @@ public class Main extends Entry {
 
 
             } else if (entryInput == 2) {
+                // Remove an entry
+
                 newAddy.delete(sc.nextInt());
-                break;
+
             } else if (entryInput == 3) {
+                // Search for an entry
+
+
+            } else if (entryInput == 4) {
+                // Printing the address book
+
                 Entry[] listOfEntries = newAddy.viewAll();
 
                 for (int i = 0; i < listOfEntries.length; i++) {
                     System.out.println(listOfEntries[i].toString());
                 }
-
-                break;
-            } else if (entryInput == 4) {
-                System.out.println("4");
             } else if (entryInput == 5) {
-                System.out.println("5");
+                //Deleting the address book
+
+                newAddy = null;
+                System.out.println("Address book has been successfully cleared.");
+
             } else if(entryInput > 6 | entryInput < 1){
-                System.out.println("Invalid");
+                System.out.println("Invalid response, please try again.");
             }
         }
         while (entryInput != 6);
